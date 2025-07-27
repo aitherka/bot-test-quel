2132af3edb34cdd973bfdc9932c1c78ed972a3ba
aitherkaaaaa <aighterstuff@gmail.com>
2025-07-27 15:56:56 +0300

    Update interactions.js

--- api/interactions.js
+++ api/interactions.js
@@ -1,39 +1,45 @@
-import { InteractionType, verifyKey } from 'discord-interactions';
+import { InteractionType, InteractionResponseType, verifyKey } from 'discord-interactions';
 
-const PUBLIC_KEY = 'c98b843d6b260378cb07c118ae6d8a76d085026349964bb2af951af627470800'; // Replace this!
+const PUBLIC_KEY = 'c98b843d6b260378cb07c118ae6d8a76d085026349964bb2af951af627470800';
 
-export default async (req, res) => {
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed');
+  }
+
   const signature = req.headers['x-signature-ed25519'];
   const timestamp = req.headers['x-signature-timestamp'];
+
   const rawBody = await getRawBody(req);
+  const isValid = verifyKey(rawBody, signature, timestamp, PUBLIC_KEY);
 
-  const isValidRequest = verifyKey(rawBody, signature, timestamp, PUBLIC_KEY);
-
-  if (!isValidRequest) {
-    return res.status(401).send('Bad request signature.');
+  if (!isValid) {
+    return res.status(401).send('Bad request signature');
   }
 
   const interaction = JSON.parse(rawBody.toString());
 
-  // Respond to ping
   if (interaction.type === InteractionType.PING) {
-    return res.status(200).json({ type: InteractionType.PING });
+    return res.status(200).json({ type: InteractionResponseType.PONG });
   }
 
-  // Respond to /howgay
-  if (
-    interaction.type === InteractionType.APPLICATION_COMMAND &&
-    interaction.data.name === 'howgay'
-  ) {
-    const percent = Math.floor(Math.random() * 101);
-    return res.status(200).json({
-      type: 4,
-      data: { content: `🌈 You are ${percent}% gay!` },
-    });
+  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    if (interaction.data.name === 'howgay') {
+      const percentage = Math.floor(Math.random() * 101);
+      return res.status(200).json({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `🌈 You are ${percentage}% gay!`
+        }
+      });
+    }
   }
 
-  return res.status(400).send('Unknown interaction type.');
-};
+  return res.status(200).json({
+    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    data: { content: "Unknown command." }
+  });
+}
 
 async function getRawBody(req) {
   return new Promise((resolve, reject) => {
